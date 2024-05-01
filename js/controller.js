1a7c97211e27c2fa6d0ba43619a25109698ce53a
Vladislav Skhomenko <vskhoma@gmail.com>
2024-05-02 02:20:37 +0500

    реализовал получение товаров из JSON и их рендер на странице

---
+++ js/controller.js
@@ -0,0 +1,13 @@
+import ProductsModel from './products/model.js'
+import * as productsView from './products/view.js'
+
+const productsModel = new ProductsModel()
+
+
+
+async function getAndRenderProducts() {
+    await productsModel.loadProducts()
+    productsView.renderProducts(productsModel.products)
+}
+
+getAndRenderProducts()
